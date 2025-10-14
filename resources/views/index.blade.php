c9caa30b161ea42f468738d08f1b145a1e8795f3
Andreas B. <andreas.brodner@t-online.de>
2025-10-14 10:17:18 +0200

    setup basic views

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,51 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Geländespiel') }}</title>
+    <title>{{ config('app.name', 'Geländespiel') }}</title>
 
-        <!-- Styles / Scripts -->
-        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-            @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @endif
-    </head>
-    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-        
-    </body>
+    <!-- Styles / Scripts -->
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endif
+</head>
+<body class="bg-neutral-800 text-neutral-100 flex items-center justify-center min-h-svh text-center">
+    <div class="flex flex-col md:max-w-80 gap-2 m-8">
+        <h1 class="text-4xl sticky top-3">Geländespiel</h1>
+        <div class="flex flex-col bg-neutral-700 rounded-lg p-2 gap-1">
+            <h2 class="text-2xl">Punktestand</h2>
+            <div class="flex flex-row gap-2">
+                <div class="flex flex-col flex-1">
+                    <span>Studenten</span>
+                    <span class="text-lg">50</span>
+                </div>
+                <div class="flex flex-col flex-1">
+                    <span>Tutoren</span>
+                    <span class="text-lg">20</span>
+                </div>
+            </div>
+            <span class="text-xs">Stand 18:37 &ndash; aktualisiert minütlich</span>
+        </div>
+        <div class="flex flex-col bg-neutral-700 rounded-lg p-2 gap-1">
+            <h2 class="text-2xl">Gruppenergebnisse</h2>
+            <p>Die Ergebnisse der einzelnen Gruppen können <a href="/results.php" class="text-blue-400 underline">hier</a> eingesehen werden.</p>
+        </div>
+        <div class="flex flex-col bg-neutral-700 rounded-lg py-2 gap-1">
+            <h2 class="text-2xl">Pfade</h2>
+            <select name="groupSelect" id="groupSelect" class="border-y outline-none">
+                @for ($i = 0; $i < 25; $i++)
+                    <option value="group{{ $i }}" class="text-black">Gruppe {{ $i }}</option>
+                @endfor
+            </select>
+        </div>
+        <div class="flex flex-col">
+            <span>Quicklinks:</span>
+            <div class="flex flex-col">
+                <a href="/tutor.php" class="text-blue-400 underline">Tutor Login</a>
+            </div>
+        </div>
+    </div>
+</body>
 </html>
